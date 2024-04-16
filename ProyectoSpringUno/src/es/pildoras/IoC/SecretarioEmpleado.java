3675099e0e92be02c5df9918ec5e824f9d824af3
Jonathan Daniel Tanasa Popescu (ext) <jonathan.tanasa.external@eviden.com>
2024-04-16 13:23:46 +0200

    tarea 2.7.4

---
+++ ProyectoSpringUno/src/es/pildoras/IoC/SecretarioEmpleado.java
@@ -0,0 +1,11 @@
+package es.pildoras.IoC;
+
+public class SecretarioEmpleado implements Empleados {
+
+	@Override
+	public String getTareas() {
+		// TODO Auto-generated method stub
+		return "Gestionar la agenda de los Jefes";
+	}
+
+}
---
+++ ProyectoSpringUno/src/es/pildoras/IoC/UsoEmpleados.java
@@ -0,0 +1,15 @@
+package es.pildoras.IoC;
+
+public class UsoEmpleados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Empleados Empleado1 = new DirectorEmpleado();
+		
+		System.out.println(Empleado1.getTareas());
+		
+		
+	}
+
+}
